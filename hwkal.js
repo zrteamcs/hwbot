019e20babaf93cd3ddff0bbe3cb3a31f7d4238b9
zrteamcs <134378629+zrteamcs@users.noreply.github.com>
2023-08-08 09:30:45 -0700

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -15,20 +15,20 @@ const twitter = require("./baseikal/download/twitter")
 global.baileys = require('@whiskeysockets/baileys') 
 global.adiwajshing = require('@adiwajshing/baileys') 
 
-global.gr = 'https://chat.whatsapp.com/LHEV9GKbWiVDMRc8CWR10t'
-global.ig = '@HW MODS WA' // ubah aja
-global.email = 'denyp857@gmail.com' //serah
+global.gr = '-'
+global.ig = '@zrteam' // ubah aja
+global.email = 'zrteamcs@gmail.com' //serah
 global.region = 'indonesia' // serah
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'zrteam' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
-global.owner = ['6281214281312'] // ubah aja pake nomor lu
+global.owner = ['6283127596164'] // ubah aja pake nomor lu
 //==========================BY Hw Mods=======================//
 global.keyopenai = `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`
 //====================BY Hw Mods=============================//
-global.botname = 'HW MODS WA' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.packname = 'HW MODS WA' // ubah aja ini nama sticker
-global.author = 'HW MODS WA' // ubah aja ini nama sticker
+global.botname = 'Zemora' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.packname = 'Zemora' // ubah aja ini nama sticker
+global.author = 'Zemora' // ubah aja ini nama sticker
 global.prefa = ['','!','.',',','🐤','🗿']
 global.sessionName = 'haikal' //Gausah Juga
 global.sp = '⭔' // Gausah Juga
